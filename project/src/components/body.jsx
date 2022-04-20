b90c5351f0467ffbab3283bce9474dbb9bfb24c5
Swathi <swathi709357@gmail.com>
2022-04-20 17:05:41 +0530

    commit

--- project/src/components/body.jsx
+++ project/src/components/body.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import styled from 'styled-components'
 import './nav.css'
+const Image = styled.img`
+width:450px`
 function Body() {
   return (
     <div>
@@ -78,6 +81,97 @@ function Body() {
          <h1>Grow with Mailchimp</h1>
          <p>We’re built for growing businesses. Put our marketing and sales tools behind your idea, dream, brand, or business and we’ll help you share it with the world.</p>
      </div>
+        <div style={{
+            display: "flex",
+            padding: "20px"
+        }}>
+            <div style={{
+                width: "50%",
+                padding: "10px",
+                
+            }}>
+                <Image src="https://eep.io/images/yzco4xsimv0y/2V2qyFsIJhzeCTHxRqOrkj/b9291dc9b867d3c65f855ab53cbd1e90/mtsb_market_your_business_floats__2_.png?w=760&fm=webp&q=80" alt="" />
+            </div>
+            <div style={{
+                width: "48%",
+                padding: "100px",
+                textAlign: "left"
+
+            }}>
+                <h1>Market your business</h1>
+                <p>Send the right messages on all the right channels. Build emails, social ads, landing pages, postcards, and more from one place.</p>
+            </div>
+        </div>
+        <div style={{
+            display: "flex",
+            padding: "20px"
+        }}>
+            <div style={{
+                width: "48%",
+                padding: "100px",
+                textAlign: "left"
+            }}>
+                <h1>Get your business online</h1>
+                <p>Build a beautiful website with a custom domain, launch an online store, and allow customers to book appointments directly from your website.</p>
+            </div>
+                <div style={{
+                    width: "50%",
+                    
+                }}>
+                    <Image src="https://eep.io/images/yzco4xsimv0y/6nQ4AQKdhqdEtbyt0ATvQI/83fa92b48a2a8881a8736e86f4c4e5fd/mtsb_get_your_business_online_floats__2_.png?w=760&fm=webp&q=80" alt="" />
+                </div>
+        </div>
+        <div style={{
+            display: "flex",
+            padding: "20px"
+        }}>
+            <div style={{
+                width: "50%",
+                
+            }}>
+                <Image src="https://eep.io/images/yzco4xsimv0y/2a6oymKQXlg4E71qZA8yjJ/70ff4711985b17539515ba6faa956275/mtsb_cjb_floats__3_.png?w=760&fm=webp&q=80" alt="" />
+            </div>
+            <div style={{
+                width: "48%",
+                padding: "100px",
+                textAlign: "left"
+            }}>
+                <h1>Automate your marketing</h1>
+                <p>Add a personal touch without the personal effort through automated messages that reach your customers at exactly the right moments.</p>
+            </div>
+        </div>
+        <div className='pics'>
+            <div>
+                <img src="https://eep.io/images/yzco4xsimv0y/01BcV4OFnqHv593jCOAoe5/14cc7d3f6e3baf94d83f944891f6f9a2/78_Audience-Management_AUI.png?w=160&fm=webp&q=80" alt="" />
+                <h2>Audience </h2>
+                <p>Put your people at the heart of your marketing with tools that help you get to know your audience and insights that can help drive better results.</p>
+            </div>
+            <div>
+                <img src="https://eep.io/images/yzco4xsimv0y/01BcV4OFnqHv593jCOAoe5/14cc7d3f6e3baf94d83f944891f6f9a2/78_Audience-Management_AUI.png?w=160&fm=webp&q=80" alt="" />
+                <h2>Creative tools</h2>
+                <p>Put your people at the heart of your marketing with tools that help you get to know your audience and insights that can help drive better results.</p>
+            </div>
+            <div>
+                <img src="https://eep.io/images/yzco4xsimv0y/01BcV4OFnqHv593jCOAoe5/14cc7d3f6e3baf94d83f944891f6f9a2/78_Audience-Management_AUI.png?w=160&fm=webp&q=80" alt="" />
+                <h2>management</h2>
+                <p>Put your people at the heart of your marketing with tools that help you get to know your audience and insights that can help drive better results.</p>
+            </div>
+        </div>
+        <div style={{background:'#ffe01b', padding:'8%'}}className='million'>
+            <h1>Lets do this</h1>
+            <button
+             
+              style={{
+                padding: "16px 36px",
+                background: " #007C89",
+                color: "#FFF",
+                border: "none",
+                marginLeft:'45%'
+              }}
+            >
+              Sign Up
+            </button>
+        </div>
     </div>
 
   )
