89068ef00fea871c080ddfcf2a8a23f35eb08e90
Swathi <swathi709357@gmail.com>
2022-04-25 18:52:16 +0530

    commit

--- project/src/components/SearchPage.jsx
+++ project/src/components/SearchPage.jsx
@@ -3,12 +3,37 @@ import './nav.css'
 import { Pagination } from "@mui/material";
 import ResponsiveAppBar from "./Nav";
 import { Footer } from "./Footer";
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import Box from '@mui/material/Box';
+import { storeData } from '../redux/storeData/action';
+
 const SearchPage = () => {
   const [search, setSearch] = useState("");
-  
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
   console.log(search)
 const [arr,setArr] = useState(null)
+const fetchData = async () => {
+  return axios({
+    url: `https://mailchimpabc.herokuapp.com/data?_page=${page}&_limit=10`,
+    method: 'GET',
+    params: {},
+  })
+    .then((response) => {
+      // const data =(response.data)
 
+      console.log(response.data)
+    dispatch(storeData(response.data));
+    })
+
+    .catch((error) => {});
+};
+
+
+useEffect(() => {
+  fetchData();
+}, [page]);
 
 const getData = () => 
 fetch("https://mailchimpabc.herokuapp.com/data")
@@ -43,9 +68,18 @@ useEffect(() => {
          
          <hr/>
          </div>
+         
        )
      })
    }
+   <Box py={4} display="flex" justifyContent="center">
+        <Pagination
+          count={10}
+          color="secondary"
+          variant="outlined"
+          onChange={(e, value) => setPage(value)}
+        />
+      </Box>
     
    <Footer/>
   </div>;
